0ed84df94f414152b387b53403b4445d77295500
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2022-01-19 21:50:08 +0100

    Initial Commit

---
+++ Todoist/TodoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistApp.swift
+//  Todoist
+//
+//  Created by Frederico Kuckelhaus on 19.01.22.
+//
+
+import SwiftUI
+
+@main
+struct TodoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

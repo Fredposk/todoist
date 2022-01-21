8697218b35d19384329c8ea108d06650c5de3468
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2022-01-21 21:09:08 +0100

    initial view preloads example items

---
+++ Todoist/HomeView.swift
@@ -0,0 +1,33 @@
+//
+//  HomeView.swift
+//  Todoist
+//
+//  Created by Frederico Kuckelhaus on 21.01.22.
+//
+
+import SwiftUI
+
+
+
+struct HomeView: View {
+    @EnvironmentObject var dataController: DataController
+
+
+    var body: some View {
+        NavigationView{
+            VStack {
+                Button("add data") {
+                    dataController.deleteAll()
+                    try? dataController.createSampleData()
+                }
+            }
+            .navigationTitle("Home")
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
